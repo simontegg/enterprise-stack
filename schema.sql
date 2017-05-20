7d059749846d2bb35f17af2e5f426fa4322382ad
simontegg <simon@enspiral.com>
2017-05-20 16:35:45 +1200

    recursive-view

--- schema.sql
+++ schema.sql
@@ -6,110 +6,83 @@ CREATE SCHEMA valueflows_private;
 
 CREATE extension IF NOT exists "uuid-ossp";
 
--- person
-CREATE TABLE valueflows.person (
-    PRIMARY KEY (id),
-    id          UUID          DEFAULT uuid_generate_v1mc()  NOT NULL, 
-    first_name  VARCHAR(80)                                 NOT NULL, 
-    last_name   VARCHAR(80)                                 NOT NULL,
-    img         VARCHAR(200),
-    created_at  TIMESTAMP     WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now()),
-    updated_at  TIMESTAMP     WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now())
-);
+-- agent
+CREATE TYPE valueflows.agent_type AS ENUM (
+    'Person',
+    'Organization'
+)
 
-COMMENT ON TABLE valueflows.person is 'A user.';
-COMMENT ON COLUMN valueflows.person.id is 'The primary unique identifier for the person.';
-COMMENT ON COLUMN valueflows.person.first_name is 'The person’s first name.';
-COMMENT ON COLUMN valueflows.person.last_name is 'The person’s last name.';
-COMMENT ON COLUMN valueflows.person.img is 'A url for an image representing the person';
-COMMENT ON COLUMN valueflows.person.created_at is 'The time this person was created.';
-COMMENT ON COLUMN valueflows.person.updated_at is 'The time this person was updated.';
-
--- organization
-CREATE TABLE valueflows.organization (
+CREATE TABLE valueflows.agent (
     PRIMARY KEY (id),
     id              UUID          DEFAULT uuid_generate_v1mc()  NOT NULL, 
-    name            TEXT                                        NOT NULL,
-    classification  TEXT,      
+    agent_type      valueflows.agent_type                       NOT NULL,
+    classification  VARCHAR(80),
     img             VARCHAR(200),
+    name            VARCHAR(80),
+    first_name      VARCHAR(80), 
+    last_name       VARCHAR(80),
     created_at      TIMESTAMP     WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now()),
     updated_at      TIMESTAMP     WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now())
 );
 
-COMMENT ON TABLE valueflows.organization is 'An organization';
-COMMENT ON COLUMN valueflows.organization.id is 'The primary unique identifier for the person.';
-COMMENT ON COLUMN valueflows.organization.name is 'The name of the organization';
-COMMENT ON COLUMN valueflows.organization.classification is 'The kind of organization';
-COMMENT ON COLUMN valueflows.organization.img is 'A url for an image representing the organization';
-COMMENT ON COLUMN valueflows.organization.created_at is 'The time this organization was created.';
-COMMENT ON COLUMN valueflows.organization.updated_at is 'The time this organization was updated.';
+COMMENT ON TABLE valueflows.agent is 'A person or organisation';
+COMMENT ON COLUMN valueflows.agent.id is 'The primary unique identifier for the agent.';
+COMMENT ON COLUMN valueflows.agent.img is 'A url for an image representing the agent';
+COMMENT ON COLUMN valueflows.agent.name is 'The name of the ';
+COMMENT ON COLUMN valueflows.agent.classification is 'The kind of ';
+COMMENT ON COLUMN valueflows.agent.first_name is 'The person’s first name.';
+COMMENT ON COLUMN valueflows.agent.last_name is 'The person’s last name.';
+COMMENT ON COLUMN valueflows.agent.created_at is 'The time this agent was created.';
+COMMENT ON COLUMN valueflows.agent.updated_at is 'The time this agent was updated.';
 
--- memberships
-CREATE TABLE valueflows.membership (
-    PRIMARY KEY (person_id, organization_id),
-    person_id       UUID                                    NOT NULL,
-                    CONSTRAINT person_id_fkey FOREIGN KEY (person_id)
-                        REFERENCES valueflows.person (id) MATCH SIMPLE
-                        ON DELETE CASCADE ON UPDATE CASCADE,
-    organization_id UUID                                    NOT NULL,
-                    CONSTRAINT organization_id_fkey FOREIGN KEY (organization_id)
-                        REFERENCES valueflows.person (id) MATCH SIMPLE
-                        ON DELETE CASCADE ON UPDATE CASCADE,
-    is_admin        BOOLEAN   DEFAULT FALSE                 NOT NULL,
-    created_at      TIMESTAMP WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now()),
-    updated_at      TIMESTAMP WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now())
+-- person
+CREATE VIEW valueflows.person AS
+    SELECT id, img, first_name, last_name, created_at, updated_at
+    FROM valueflows.agent AS a
+    WHERe a.agent_type = 'Person';
+
+-- organization
+CREATE VIEW valueflows.organization AS
+    SELECT id, img, name, classification, created_at, updated_at
+    FROM valueflows.agent AS a
+    WHERe a.agent_type = 'Organization';
+
+
+CREATE TABLE valueflows.relationship (
+    PRIMARY KEY (subject_id, object_id, relationship_type, degrees),
+    relationship_type   VARCHAR(256)  NOT NULL,
+    subject_id          UUID          NOT NULL,
+                        CONSTRAINT subject_id_fkey FOREIGN KEY (subject_id)
+                             REFERENCES valueflows.agent (id) MATCH SIMPLE
+                             ON DELETE CASCADE ON UPDATE CASCADE,
+    object_id           UUID          NOT NULL,
+                        CONSTRAINT object_id_fkey FOREIGN KEY (object_id)
+                            REFERENCES valueflows.agent (id) MATCH SIMPLE
+                            ON DELETE CASCADE ON UPDATE CASCADE,
+    created_at          TIMESTAMP WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now()),
+    updated_at          TIMESTAMP WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now())
 );
 
-COMMENT ON TABLE valueflows.membership is 'A relationship indicating that a person is a member of an organization';
-COMMENT ON COLUMN valueflows.membership.person_id is 'A uuid referencing the person who is a member';
-COMMENT ON COLUMN valueflows.membership.person_id is 'A uuid referencing the organisation of which the person is a member';
-COMMENT ON COLUMN valueflows.membership.created_at is 'The time this relationship was created.';
-COMMENT ON COLUMN valueflows.membership.updated_at is 'The time this relationship was updated.';
+COMMENT ON TABLE valueflows.relationship IS 'A relationship indicating that a person is a member of an organization';
+COMMENT ON COLUMN valueflows.relationship.subject_id IS 'A uuid that refernces the subject (agent) of the relationship' ;
+COMMENT ON COLUMN valueflows.relationship.object_id IS 'A uuid that references the object (agent) of the relationship';
+COMMENT ON COLUMN valueflows.relationship.classification IS 'The type of relationship.';
+COMMENT ON COLUMN valueflows.relationship.created_at IS 'The time this relationship was created.';
+COMMENT ON COLUMN valueflows.relationship.updated_at IS 'The time this relationship was updated.';
 
--- closure list / bridge table
--- allows a sub-organization to have multiple independent parents?
--- nested hierarchical relationships between organizations
-CREATE TABLE valueflows.organization_relationship (
-    PRIMARY KEY (subject_id, object_id, classification),
-    classification  VARCHAR(256)  NOT NULL,
-    subject_id      UUID          NOT NULL,
-                    CONSTRAINT subject_id_fkey FOREIGN KEY (subject_id)
-                         REFERENCES valueflows.organization (id) MATCH SIMPLE
-                         ON DELETE CASCADE ON UPDATE CASCADE,
-    object_id       UUID          NOT NULL,
-                    CONSTRAINT object_id_fkey FOREIGN KEY (object_id)
-                        REFERENCES valueflows.organization (id) MATCH SIMPLE
-                        ON DELETE CASCADE ON UPDATE CASCADE,
-    depth           INT           NOT NULL,
-    created_at      TIMESTAMP WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now()),
-    updated_at      TIMESTAMP WITHOUT TIME ZONE DEFAULT timezone('utc'::text, now())
+-- views
+CREATE RECURSIVE VIEW valueflows.belongs_to (id, super) AS (
+    SELECT subject_id, object_id, created_at, updated_at
+    FROM valueflows.relationship
+    WHERE relationship_type = 'Membership'
+    UNION ALL
+    SELECT s.subject_id, b.object_id
+    FROM valueflows.relationship s, belongs_to b
+    WHERE s.object_id = b.subject_id
 );
 
-COMMENT ON TABLE valueflows.organization_relationship IS 'A relationship indicating that a person is a member of an organization';
-COMMENT ON COLUMN valueflows.organization_relationship.subject_id IS 'A uuid that references an organization to which the descendant belongs.';
-COMMENT ON COLUMN valueflows.organization_relationship.object_id IS 'A uuid that references the sub-organization.';
-COMMENT ON COLUMN valueflows.organization_relationship.classification IS 'The type of relationship.';
-COMMENT ON COLUMN valueflows.organization_relationship.depth IS 'The number of relationships between ancestor and its descendant';
-COMMENT ON COLUMN valueflows.organization_relationship.created_at IS 'The time this relationship was created.';
-COMMENT ON COLUMN valueflows.organization_relationship.updated_at IS 'The time this relationship was updated.';
 
--- functions
 
-CREATE FUNCTION valueflows.create_relationship (
-  _subject_id UUID, 
-  _object_id UUID, 
-  _classification VARCHAR(256)
-) RETURNS VOID AS $$
-    INSERT INTO valueflows.organization_relationship (subject_id, object_id, depth, classification)
-    VALUES (_subject_id, _object_id, 0, _classification);
-    INSERT INTO valueflows.organization_relationship (subject_id, object_id, depth, classification)
-    SELECT s.subject_id, o.object_id, s.depth + o.depth + 1, s.classification
-    FROM valueflows.organization_relationship o, valueflows.organization_relationship s
-    WHERE s.object_id = _object_id 
-      AND o.subject_id = _subject_id 
-      AND o.classification = _classification 
-      AND s.classification = _classification
-$$ LANGUAGE SQL;
 
 -- private
 CREATE TABLE valueflows_private.person_account (
