2d777cfd4ce59fee1b9d2f5ef4ae49b1ec8cd879
simontegg <simon@enspiral.com>
2017-06-17 16:18:44 +1200

    autosizer

--- layout/index.js
+++ layout/index.js
@@ -1,7 +1,18 @@
+const debug = require('debug')('vf:layout')
 // components
 import Head from 'next/head'
 import { Affix, Col, Menu, Icon, Row } from 'antd'
-import { Provider } from 'react-fela'
+
+// layout
+import GridLayout from 'react-grid-layout'
+import { AutoSizer } from 'react-virtualized'
+const layout = [
+  { i: 'top-corner', x: 0, y: 0, w: 1, h: 0.5, static: true },
+  { i: 'header', x: 1, y: 0, w: 8, h: 0.5, static: true },
+  { i: 'sider', x: 0, y: 0.5, w: 1, h: 5, static: true },
+  { i: 'content', x: 1, y: 0.5, w: 8, h: 5, static: true }
+]
+
 import Image from '../components/image'
 import MdDashboard from '../components/icons/md-dashboard'
 
@@ -10,53 +21,35 @@ import style from './layout.less'
 import { textColorDark, fontSizeLg } from '../styles/theme'
 import getRenderer from '../styles/fela'
 
-// layout
-import GridLayout from 'react-grid-layout'
-const layout = [
-  { i: 'top-corner', x: 0, y: 0, w: 1, h: 0.5, static: true },
-  { i: 'header', x: 1, y: 0, w: 8, h: 0.5, static: true },
-  { i: 'sider', x: 0, y: 0.5, w: 1, h: 8, static: true },
-  { i: 'content', x: 1, y: 0.5, w: 8, h: 8, static: true }
-]
-
-export default ({ children }) => (
-  <div>
+export default ({ width, height, children }) => (
+  <GridLayout className='layout' layout={layout} rowHeight={150} width={width}>
     <Head>
       <style dangerouslySetInnerHTML={{ __html: style }} />
     </Head>
-    <Provider renderer={getRenderer()}>
-      <GridLayout
-        className='layout'
-        layout={layout}
-        rowHeight={150}
-        width={2000}
-      >
-        <div key='top-corner'>
-          <Image src='/static/Accreditron.jpg' size={64} />
-        </div>
-        <div key='header'>
-          <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
-            <Menu.Item key='1' />
-          </Menu>
-        </div>
-        <div key='sider'>
-          <Affix>
-            <Menu theme='dark' mode='inline' defaultSelectedKeys={['a']}>
-              <Menu.Item key='a'>
-                <MdDashboard
-                  size={fontSizeLg}
-                  marginRight={parseInt(fontSizeLg) * 1.25}
-                  color={textColorDark}
-                />
-                <span>Dashboard</span>
-              </Menu.Item>
-            </Menu>
-          </Affix>
-        </div>
-        <div key='content'>
-          {children}
-        </div>
-      </GridLayout>
-    </Provider>
-  </div>
+    <div key='top-corner'>
+      <Image src='/static/Accreditron.jpg' size={64} />
+    </div>
+    <div key='header'>
+      <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
+        <Menu.Item key='1' />
+      </Menu>
+    </div>
+    <div key='sider'>
+      <Affix>
+        <Menu theme='dark' mode='inline' defaultSelectedKeys={['a']}>
+          <Menu.Item key='a'>
+            <MdDashboard
+              size={fontSizeLg}
+              marginRight={parseInt(fontSizeLg) * 1.25}
+              color={textColorDark}
+            />
+            <span>Dashboard</span>
+          </Menu.Item>
+        </Menu>
+      </Affix>
+    </div>
+    <div key='content'>
+      {children}
+    </div>
+  </GridLayout>
 )
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,14 @@ if (process.browser) {
 
 // import withData from '../apollo/with-data'
 import Head from 'next/head'
+import { AutoSizer } from 'react-virtualized'
+import { Provider } from 'react-fela'
 
 import Layout from '../layout'
 import OrganizationList from '../organizations'
 
 import styles from '../node_modules/antd/lib/style/index.less'
+import getRenderer from '../styles/fela'
 
 // react apollo
 // export default withData(props => (
@@ -23,14 +26,22 @@ import styles from '../node_modules/antd/lib/style/index.less'
 // ))
 
 export default props => (
-  <div>
-    <Head>
-
-      <style dangerouslySetInnerHTML={{ __html: styles }} />
-    </Head>
-    <Layout>
-      <OrganizationList />
-
-    </Layout>
-  </div>
+  <AutoSizer>
+    {({ height, width }) => {
+      const h = height || 1080
+      const w = width || 1920
+      return (
+        <Provider renderer={getRenderer()}>
+          <div>
+            <Head>
+              <style dangerouslySetInnerHTML={{ __html: styles }} />
+            </Head>
+            <Layout width={width}>
+              <OrganizationList />
+            </Layout>
+          </div>
+        </Provider>
+      )
+    }}
+  </AutoSizer>
 )
