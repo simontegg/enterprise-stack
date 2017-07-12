8a85c7cd07c141b7b7e37016e7a5ca3f2a9cc775
simontegg <simon@enspiral.com>
2017-07-13 11:15:30 +1200

    drone-fix

--- test/db/20170710082822_schema.test.js
+++ test/db/20170710082822_schema.test.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
 const path = require('path')
+const knex = require('../../db')
 
-const filepath = path.join(__dirname, '../deploy/valueflows-schema.sql')
+const filepath = path.join(__dirname, '../../db/verify/valueflows-schema.sql')
+
+  
