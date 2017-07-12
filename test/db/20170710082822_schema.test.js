2b459798138940ecdfbbc8d79a63dcbb6de0433a
simontegg <simon@enspiral.com>
2017-07-13 11:39:56 +1200

    drone-fix-1

--- test/db/20170710082822_schema.test.js
+++ test/db/20170710082822_schema.test.js
@@ -1,7 +1,23 @@
+// main
 const fs = require('fs')
 const path = require('path')
+const { promisify } = require('util')
+const readFileAsync = promisify(fs.readFile)
+
+// modules
 const knex = require('../../db')
 
-const filepath = path.join(__dirname, '../../db/verify/valueflows-schema.sql')
+test('schema', async done => {
+  const filepath = path.join(__dirname, '../../db/verify/valueflows-schema.sql')
+  const verify = await readFileAsync(filepath, 'utf8')
 
+  const result = await knex.raw(verify)
+
+  console.log(result)
+
+  expect(1).toBe(1)
+
+  
+
+})
   
